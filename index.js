f098bcdec4e7062eb7d0c98976131ef0373dda19
PriyangaV <gayathripriya.sb@gmail.com>
2020-04-06 11:25:05 +0530

    Move private info into dev

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
 
 // DB Connection
-mongoose.connect('mongodb+srv://admin:admin26@cluster0-xlswe.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
   if(err){
     process.exit(1);
     console.log('Unable to connect to database');
@@ -12,9 +18,27 @@ mongoose.connect('mongodb+srv://admin:admin26@cluster0-xlswe.mongodb.net/test?re
     console.log('Successfully connected to the database');
 });
 
-// Routing
+// Body-Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Cookie-Parser
+app.use(cookieParser());
+
+// Routing - GET
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Routing - GET
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userDate) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
 });
 
 // Listen
